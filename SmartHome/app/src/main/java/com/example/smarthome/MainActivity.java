01ce31cb9c214c9f07af87078ad40e70de1456d7
tRaihanSarkar <t.raihansarkar@gmail.com>
2019-11-05 19:09:29 +0600

    Added the SmartHome Android Project File

---
+++ SmartHome/app/src/main/java/com/example/smarthome/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.smarthome;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /** Called when the user taps the Settings button */
+    public void goToSettings(View view) {
+        // Do something in response to button
+
+    }
+
+    public void checkStatus() {
+
+    }
+}
